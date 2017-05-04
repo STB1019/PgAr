170be7cebbe462bfe3f7c326700794f5cec67205
Constantin <Con-You@users.noreply.github.com>
2017-05-04 12:46:52 +0200

    Create Interfaccia.java

---
+++ #2/Interfaccia.java
@@ -0,0 +1,11 @@
+public interface Interfaccia {
+
+	public String stampaVino();
+	
+	public long numeroVini(String nome);
+	
+	public void possibileGuadagno(String valuta);
+	
+	public Class<?> stampaFasciaVini(Object annoInizio, Object annoFine);
+	
+}
