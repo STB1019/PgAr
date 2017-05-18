8b68587a4f27e0a7fe86b6e223ef7cbb35c26270
Andrea Rossi <RedsAnDev@users.noreply.github.com>
2017-05-18 12:58:50 +0200

    Update Interfaccia.java
    Update

--- 3/Interfaccia.java
+++ 3/Interfaccia.java
@@ -1,19 +1,19 @@
 public interface Interfaccia {
 
-	public String stampaMedici(); //toString di tutti i medici presenti
+	public String stampaMedici(); //toString di tutti i medici visualizzando ID univoco, Cognome, Nome, Anno Laurea, Anno Assunzione
 
-	public void stampaOrari(int matricola); //Stama gli Orari del Medico inserito
+	public Collection<T> stampaOrari(E check); //Stampa gli Orari del Medico inserito in base all'ID, o al Nome, o all'anno di Laurea (casi di omonimia, stampa tutti i risultati) 
 
-	public void stampaAppuntamenti(int matricola); //Stampa tutti gli appuntamenti del Medico Inserito
+	public Collection<T> stampaAppuntamenti(E matricola); //Stampa tutti gli appuntamenti del Medico Inserito
 
 	public String stampaPazienti(); //Stampa tutti i Pazienti inseriti nel Programma
 
-	public void stampaAppuntamenti(int dataInizio, int dataFine); //Stampa gli appuntamenti su un certo intervallo
+	public Collection<T> stampaAppuntamenti(int dataInizio, int dataFine); //Stampa gli appuntamenti su un certo intervallo
 
-	public int stampaNumAppuntamenti(int matricola); //Stampa il numero di appuntamenti per un certo Medico
+	public  stampaNumAppuntamenti(int matricola); //Stampa il numero di appuntamenti per un certo Medico
 
 	//Ho inserito le funzionalità di base, così potete sbizzarrirvi nel creare gli altri Metodi
 
-	// ATTENZIONE: Creare metodi di Modifica di tutti i parametri (se Possibile) del vostro Programma
+	public void getTime(); //Recupera la data attuale (almeno i millisecondi). Dopo la prima esecuzione, stampa in Debug il delay time di chiamata in chiamata, partendo dalla prima volta.
 
 }
