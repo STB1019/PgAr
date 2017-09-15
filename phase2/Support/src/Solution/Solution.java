cb4a4e05d2e2b7c59811bb78aa49c629d26143ba
Andrea Rossi <code@redsandev.com>
2017-09-15 16:54:05 +0200

    Add Support

---
+++ phase2/Support/src/Solution/Solution.java
@@ -0,0 +1,14 @@
+package Solution;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		while(in.hasNext())
+			System.out.println(in.nextLine());
+	}
+
+}
